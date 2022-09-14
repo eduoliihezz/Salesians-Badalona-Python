d7d9f0ca410e948836289fab9eecac76b33177f9
Edu Olivares <93079880+eduoliihezz@users.noreply.github.com>
2022-09-14 17:26:34 +0200

    🍎🍉

---
+++ Ejercicio 1/frutas.py
@@ -0,0 +1,14 @@
+taronges = 12
+llimones = 5
+
+"""
+taronges = 5
+llimones = 12
+"""
+
+# Taronges
+a = 17 - taronges
+# Llimones
+b = 17 - llimones
+
+print (f'Hi han {a} taronges, i un totoal de {b}, llimones')
